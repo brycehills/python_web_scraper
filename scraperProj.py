e29b536b63f947a89f09686785a29c15407b7cfc
omgisthatr0man <hillsbryce@yahoo.com>
2018-07-30 17:36:00 -0700

    working scrape for name, rating, price and shipping
    additionally, added error chekcing for non existsant reviews

--- scraperProj.py
+++ scraperProj.py
@@ -20,12 +20,61 @@ page_soup = soup(page_html, "html.parser")
 # grab header 1
 page_soup.h1
 
-#grab body using html
+#grab body
 page_soup.body
 
 # grab all items containing specified tag
 containers = page_soup.findAll("div", {"class":"item-container"})
 
-for container in containers:
-    brand = container.div.div.a.img["title"]
-    print(brand)
+num_products = len(containers)
+
+info = page_soup.findAll("div", {"class":"item-info"})
+
+# ******************* begin for loop *************************************
+for i in info:
+    # grabs title of item
+    brand = i.div.a.img["title"]
+
+    # grab name block
+    nameContainer = i.findAll("a",{"class","item-title"})
+    name = nameContainer[0].text.strip("Video Card")
+
+    # grabs price area within item container block
+    priceContainer = i.findAll("li" , {"class":"price-current"})
+    # grabs actual price of item
+    price = priceContainer[0].strong.text
+
+    # grabs ratin block
+    ratingContainer = i.findAll("a", {"class","item-rating"})
+
+    # check if the container exists
+    if ratingContainer:
+        # if exists give it its proper string value
+        rating = ratingContainer[0]["title"].strip("Rating + ")
+        # append stars for readability
+        rating += " stars"
+    else:
+        # if does not exist display not found
+        rating = "No rating found"
+
+
+    shippingContainer = i.findAll("li",{"class","price-ship"})
+    shipping = shippingContainer[0].text.strip()
+
+    # iterate and print info - can only concat(+) with str
+    print("BRAND:    " + brand)
+    # print(nameContainer)
+    print("NAME:     " + name)
+    print("PRICE:    $" + price + ".00 USD")
+    print("SHIPPING: " + shipping)
+    print("RATING:   " + rating)
+    print("") # newline for readability in output
+
+    # next scrape to output file
+
+# *********************** end for loop ***********************************
+
+
+
+# syntax for findall:
+# beginning tag , following tag = label
